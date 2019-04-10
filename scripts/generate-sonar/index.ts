fdeea85d533f22060f73f2d72b29d2775513195e
Unknown <lee.david.cs@gmail.com>
2019-04-09 23:09:24 -0700

    Update project scripts

---
+++ scripts/generate-sonar/index.ts
@@ -0,0 +1,46 @@
+import { writeFile } from "fs-extra";
+import { Environment, FileSystemLoader } from "nunjucks";
+import ReadPkg, { Package } from "read-pkg";
+
+const inclusionPatterns: string[] = [
+  "**/*.test.ts",
+  "**/*.test.tsx"
+];
+
+const exclusionPatterns: string[] = [
+  "build/**/*",
+  "config/**/*",
+  "coverage/**/*",
+  "logs/**/*",
+  "node_modules/**/*",
+  "public/**/*",
+  "scripts/**/*",
+  "test-reports/**/*",
+  "**/*test.ts",
+  "**/*test.tsx",
+  "**/*.js",
+  "**/*.jsx",
+  "**/*.d.ts",
+  "**/*.d.tsx"
+];
+
+/**
+ * @function main
+ * @returns { void }
+ * @description Generates a sonar-project.properties file for SonarQube static analysis
+ */
+const main = async (): Promise<never> => {
+  const pkg: Package = await ReadPkg();
+  const environmnent: Environment = new Environment(new FileSystemLoader("scripts/generate-sonar"));
+
+  const inclusions: string = inclusionPatterns.join(",");
+  const exclusions: string = exclusionPatterns.join(",");
+
+  const fileContents: string = environmnent.render("sonar-project.njk", { exclusions, inclusions, pkg });
+
+  await writeFile("sonar-project.properties", fileContents);
+
+  return process.exit(0);
+};
+
+main();
