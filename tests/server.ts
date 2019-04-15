6e3840f06803c20f3bcb7261de0f316651bc8c78
Unknown <lee.david.cs@gmail.com>
2019-04-15 00:23:54 -0700

    Add server tests

---
+++ tests/server.ts
@@ -0,0 +1,28 @@
+import Http from "http";
+import { Server } from "../server";
+
+const PORT: number = Number(process.env.port) || 3001;
+
+const INIT_SERVER_SPIN_TIMEOUT: number = 120000;
+
+describe("Server", () => {
+	const server: Server = new Server(PORT);
+
+	afterEach(async () => {
+		await server.stop();
+	});
+
+	it("Should be a constructor", () => {
+		expect(typeof Server).toBe("function");
+	});
+
+	it("Should run the server", async () => {
+		await server.run();
+
+		expect(server.isRunning).toBe(true);
+	}, INIT_SERVER_SPIN_TIMEOUT);
+
+	it("Should return an instance of an http server", () => {
+		expect(server.instance instanceof Http.Server).toBe(true);
+	});
+});
